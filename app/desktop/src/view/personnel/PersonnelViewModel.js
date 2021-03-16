74b33154a30f788057648d7bbf1a8aef3db1c706
zshoket <zohaibshoket5@gmail.com>
2021-03-16 16:18:28 +0100

    updated RiskMatrix

--- app/desktop/src/view/personnel/PersonnelViewModel.js
+++ app/desktop/src/view/personnel/PersonnelViewModel.js
@@ -2,6 +2,19 @@ Ext.define('SORISMA.view.personnel.PersonnelViewModel', {
 	extend: 'Ext.app.ViewModel',
 	alias: 'viewmodel.personnelviewmodel',
 	data: {
-		name: 'SORISMA'
+		data: {
+			per_id: 0,
+			name: "",
+			unternehmen: "",
+			dimension: "",
+			einordnungKette: "",
+			kurzbeschreibung: "",
+			reifegrad: "",
+			nutzenversprechen: "",
+			herausforderungen: "",
+			veraenderungenMensch: "",
+			veraenderungenOrganisation: "",
+			veraenderungenTechnologie: "",
+		}
 	}
 });
--- app/desktop/src/view/risikoMatrix/risikoMatrix.js
+++ app/desktop/src/view/risikoMatrix/risikoMatrix.js
@@ -1,23 +1,22 @@
 Ext.define("SORISMA.view.risikoMatrix.risikoMatrix", {
-  extend: "Ext.panel.Panel",
-  xtype: "risikomatrix",
-  requires: [
-    "SORISMA.view.risikoMatrix.risikoMatrixController",
-    "SORISMA.view.risikoMatrix.risikoMatrixModel",
-    "Ext.form.FieldSet",
-    "Ext.form.field.*",
-    "Ext.navigation.View",
-    "Ext.container.*",
-  ],
+    extend: "Ext.panel.Panel",
+    xtype: "risikomatrix",
+    requires: [
+        "SORISMA.view.risikoMatrix.risikoMatrixController",
+        "SORISMA.view.risikoMatrix.risikoMatrixModel",
+        "Ext.form.FieldSet",
+        "Ext.form.field.*",
+        "Ext.navigation.View",
+        "Ext.container.*",
+    ],
 
-  controller: "risikomatrix-risikomatrix",
-  viewModel: {
-    type: "risikomatrix-risikomatrix",
-  },
-  scrollable: true,
+    controller: "risikomatrix-risikomatrix",
+    viewModel: {
+        type: "risikomatrix-risikomatrix",
+    },
+    scrollable: true,
 
-  defaultType: "panel",
+    defaultType: "panel",
 
-  html:
-    '<iframe src="html/Riskmatrix.html" scrolling="no" height="900" width="100%"></iframe>',
+    html: '<iframe src="html/graph.html" scrolling="no" height="900" width="100%"></iframe>',
 });
